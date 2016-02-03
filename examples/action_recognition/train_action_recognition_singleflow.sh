bf65ae151c42936f43e8050f4843fe1458926e58
mathrho <author@corpmail.com>
2016-02-03 22:37:17 +0100

    add flow train code

--- examples/action_recognition/train_action_recognition_singleflow.sh
+++ examples/action_recognition/train_action_recognition_singleflow.sh
@@ -2,7 +2,7 @@
 
 GOOGLE_LOG_DIR=models/action_recognition/log \
     mpirun -np 4 \
-    cmake_build/install/bin/caffe train \
+    build/install/bin/caffe train \
     --solver=models/action_recognition/vgg_16_singleflow_solver.prototxt \
     --weights=models/action_recognition/vgg_16_action_rgb_pretrain.caffemodel
 
