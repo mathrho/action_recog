edcd23345568c91b12ef2a452da4c954197a17c4
mathrho <author@corpmail.com>
2016-02-08 15:02:02 +0100

    add myflow

---
+++ examples/action_recognition/train_action_recognition_myflow_2.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env sh
+
+GOOGLE_LOG_DIR=models/action_recognition/log \
+    mpirun -np 4 \
+    build/install/bin/caffe train \
+    --solver=models/action_recognition/vgg_16_myflow_solver_2.prototxt \
+    --weights=models/action_recognition/snapshot_myflow_lr0.005_1_lr0.001/lzy_action_recognition_16_split1_myflow_iter_20000.caffemodel
