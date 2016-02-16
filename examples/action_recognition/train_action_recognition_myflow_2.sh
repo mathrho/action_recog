dd5527b9e73fc7a54ad792d6913c9c1521176dfe
mathrho <author@corpmail.com>
2016-02-16 13:11:23 +0100

    add option singleflow in data_transformer

--- examples/action_recognition/train_action_recognition_myflow_2.sh
+++ examples/action_recognition/train_action_recognition_myflow_2.sh
@@ -4,5 +4,5 @@ GOOGLE_LOG_DIR=models/action_recognition/log \
     mpirun -np 4 \
     build/install/bin/caffe train \
     --solver=models/action_recognition/vgg_16_myflow_solver_2.prototxt \
-    --weights=models/action_recognition/snapshot_myflow_lr0.005_1_lr0.0005/lzy_action_recognition_vgg_16_split1_singleflow_iter_38000.caffemodel
+    --weights=models/action_recognition/snapshot_myflow_lr0.005_1_lr0.0005/lzy_action_recognition_vgg_16_split1_singleflow_iter_37000.caffemodel
 
