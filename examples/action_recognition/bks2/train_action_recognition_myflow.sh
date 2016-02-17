2069b80497c3792c0d8ce40c8e5a2107fb4bc903
Zhenyang <mathrho@vpn-staff-146-50-68-115.vpn.uva.nl>
2016-02-17 02:12:24 +0100

    renew myflow

--- examples/action_recognition/bks2/train_action_recognition_myflow.sh
+++ examples/action_recognition/bks2/train_action_recognition_myflow.sh
@@ -4,5 +4,5 @@ GOOGLE_LOG_DIR=models/action_recognition/log \
     mpirun -np 4 \
     build/install/bin/caffe train \
     --solver=models/action_recognition/vgg_16_myflow_solver.prototxt \
-    --weights=models/action_recognition/vgg_16_action_myflow_pretrain.caffemodel
+    --weights=models/action_recognition/vgg_16_action_rgb_pretrain.caffemodel
 
