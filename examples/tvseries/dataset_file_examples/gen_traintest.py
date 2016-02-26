4e91690dd18e6119326d2234358501fb17268eea
mathrho <author@corpmail.com>
2016-02-26 21:14:55 +0100

    add tvseries code

---
+++ examples/tvseries/dataset_file_examples/gen_traintest.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python
+
+import sys, os
+from os import listdir
+from os.path import isfile, join
+import numpy as np
+
+
+if __name__ == "__main__":
+
+    ###
+    filename = './annot_caffe_train_origin.txt'
+    fp_ = open('./annot_caffe_train.txt', 'w')
+    with open(filename) as fp:
+        for line in fp:
+            splits = line.rstrip().split(' ')
+            label = int(splits[2]) - 1
+            fp_.write(splits[0]+' '+splits[1]+' '+str(label))
+    fp_.close()
+
+    ###
+    filename = './annot_caffe_test_origin.txt'
+    fp_ = open('./annot_caffe_test.txt', 'w')
+    with open(filename) as fp:
+        for line in fp:
+            splits = line.rstrip().split(' ')
+            label = int(splits[2]) - 1
+            fp_.write(splits[0]+' '+splits[1]+' '+str(label))
+    fp_.close()
+
+    ###
+    filename = './annot_caffe_val_origin.txt'
+    fp_ = open('./annot_caffe_val.txt', 'w')
+    with open(filename) as fp:
+        for line in fp:
+            splits = line.rstrip().split(' ')
+            label = int(splits[2]) - 1
+            fp_.write(splits[0]+' '+splits[1]+' '+str(label))
+    fp_.close()
+
+    print '*********** PROCESSED ALL *************'
+
--- examples/tvseries/train_action_recognition_rgb.sh
+++ examples/tvseries/train_action_recognition_rgb.sh
@@ -4,5 +4,5 @@ GOOGLE_LOG_DIR=models/tvseries/log \
     mpirun -np 1 \
     build/install/bin/caffe train \
     --solver=models/tvseries/vgg_16_rgb_solver.prototxt \
-    --weights=models/tvseries/vgg_16_action_rgb_pretrain.caffemodel
+    --weights=models/tvseries/lzy_action_recognition_vgg_16_split1_rgb_iter_10K.caffemodel
 
