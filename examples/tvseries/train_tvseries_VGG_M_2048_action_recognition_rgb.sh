e1f46a772c4fec7a42114298d8a2f8d918849e31
mathrho <author@corpmail.com>
2016-02-28 22:21:28 +0100

    add tvseries VGG_M code

--- examples/tvseries/train_tvseries_VGG_M_2048_action_recognition_rgb.sh
+++ examples/tvseries/train_tvseries_VGG_M_2048_action_recognition_rgb.sh
@@ -4,5 +4,5 @@ GOOGLE_LOG_DIR=models/tvseries/log \
     mpirun -np 1 \
     build/install/bin/caffe train \
     --solver=models/tvseries/VGG_M_2048_rgb_solver.prototxt \
-    --weights=models/tvseries/cuhk_ucf101_spatial_v2.caffemodel
+    --weights=models/tvseries/VGG_CNN_M_2048.caffemodel
 
