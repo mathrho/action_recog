c6d9f9b1fdea8217969a1519b9836216e5bbb158
Zhenyang <mathrho@vpn-staff-146-50-68-115.vpn.uva.nl>
2016-02-29 02:57:46 +0100

    add hmdb exp

--- examples/hmdb51/dataset_file_examples/gen_traintest.py
+++ examples/hmdb51/dataset_file_examples/gen_traintest.py
@@ -10,7 +10,6 @@ import glob
 if __name__ == "__main__":
 
     dataset = '/home/zhenyang/Workspace/data/HMDB51'
-
     allclasses = sorted(glob.glob(join(dataset, 'test_train_splits', '*_test_split1.txt')))
 
     fp_train = open(join(dataset, 'train1.txt'), 'w')
@@ -24,9 +23,9 @@ if __name__ == "__main__":
                 # check train or test
                 if label > 0:
                     if label == 1:
-                        fp_train.write(filename + ' ' + str(c) + '\n')
+                        fp_train.write(filename+ ' '+str(c)+'\n')
                     elif label == 2:
-                        fp_test.write(filename + ' ' + str(c) + '\n')
+                        fp_test.write(filename+ ' '+str(c)+'\n')
                     else:
                         print 'error!'
                         exit()
@@ -34,6 +33,49 @@ if __name__ == "__main__":
     fp_train.close()
     fp_test.close()
 
+    max_duration = 0
+    ##
+    fp_train = open('./train_rgb_split1.txt', 'w')
+    with open(join(dataset, 'train1.txt')) as fp:
+        for line in fp:
+            splits = line.rstrip().split(' ')
+            filename = splits[0]
+            label = int(splits[1])
+            filename_ = os.path.splitext(os.path.basename(filename))[0]
+            filefullname = join(dataset, 'features', 'flow_tvl1_gpu', filename_)
+
+            frames = glob.glob(join(filefullname, 'image_*.jpg'))
+            duration = len(frames)
+            #print 'Duration: ', duration
+
+            fp_train.write(filefullname+' '+str(duration)+' '+str(label)+'\n')
+
+            if duration > max_duration:
+                max_duration = duration
+    fp_train.close()
+
+    ##
+    fp_test = open('./val_rgb_split1.txt', 'w')
+    with open(join(dataset, 'test1.txt')) as fp:
+        for line in fp:
+            splits = line.rstrip().split(' ')
+            filename = splits[0]
+            label = int(splits[1])
+            filename_ = os.path.splitext(os.path.basename(filename))[0]
+            filefullname = join(dataset, 'features', 'flow_tvl1_gpu', filename_)
+
+            frames = glob.glob(join(filefullname, 'image_*.jpg'))
+            duration = len(frames)
+            #print 'Duration: ', duration
+
+            fp_test.write(filefullname+' '+str(duration)+' '+str(label)+'\n')
+
+            if duration > max_duration:
+                max_duration = duration
+    fp_test.close()
+
+   
+    print 'Video Max Duration: ', max_duration
 
     print '*********** PROCESSED ALL *************'
 
