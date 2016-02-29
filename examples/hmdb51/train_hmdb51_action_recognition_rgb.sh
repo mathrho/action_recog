effdeefdbeae3bd046b1675ebcd5e4aa144ce896
mathrho <author@corpmail.com>
2016-02-29 15:39:32 +0100

    add hmdb code

--- examples/hmdb51/train_hmdb51_action_recognition_rgb.sh
+++ examples/hmdb51/train_hmdb51_action_recognition_rgb.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
 GOOGLE_LOG_DIR=models/hmdb51/log \
-    mpirun -np 1 \
+    mpirun -np 2 \
     build/install/bin/caffe train \
     --solver=models/hmdb51/vgg_16_rgb_solver.prototxt \
     --weights=models/hmdb51/lzy_action_recognition_vgg_16_split1_rgb_iter_10K.caffemodel
