7c2d95cbe39d7851c81e73dce1aff4d1281a6de0
Zhenyang <mathrho@vpn-staff-146-50-68-115.vpn.uva.nl>
2016-03-02 02:28:30 +0100

    add hmdb eval

---
+++ examples/hmdb51/dataset_file_examples/gen_iterator_metafile.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python
+
+import sys, os
+from os import listdir
+from os.path import isfile, join
+import numpy as np
+import glob
+
+
+if __name__ == "__main__":
+
+    trainset = './train_flow_split1.txt'
+    fp_train_filenames = open('/home/zhenyang/Workspace/data/HMDB51/train_filenames_split1', 'w')
+    fp_train_framenum = open('/home/zhenyang/Workspace/data/HMDB51/train_framenum_split1', 'w')
+    fp_train_labels = open('/home/zhenyang/Workspace/data/HMDB51/train_labels_split1', 'w')
+    with open(trainset) as fp:
+        for line in fp:
+            splits = line.rstrip().split(' ')
+            filename = os.path.splitext(os.path.basename(splits[0]))[0]
+            numframe = int(splits[1])
+            label = int(splits[2])
+            
+            fp_train_filenames.write(filename+'\n')
+            fp_train_framenum.write(str(numframe)+'\n')
+            fp_train_labels.write(str(label)+'\n')
+
+
+    testset = './val_flow_split1.txt'
+    fp_test_filenames = open('/home/zhenyang/Workspace/data/HMDB51/test_filenames_split1', 'w')
+    fp_test_framenum = open('/home/zhenyang/Workspace/data/HMDB51/test_framenum_split1', 'w')
+    fp_test_labels = open('/home/zhenyang/Workspace/data/HMDB51/test_labels_split1', 'w')
+    with open(testset) as fp:
+        for line in fp:
+            splits = line.rstrip().split(' ')
+            filename = os.path.splitext(os.path.basename(splits[0]))[0]
+            numframe = int(splits[1])
+            label = int(splits[2])
+            
+            fp_test_filenames.write(filename+'\n')
+            fp_test_framenum.write(str(numframe)+'\n')
+            fp_test_labels.write(str(label)+'\n')
+
+
+    print '*********** PROCESSED ALL *************'
+
