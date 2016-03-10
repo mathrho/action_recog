28128a4ff6788160193659ebe18148b5e411eca2
mathrho <author@corpmail.com>
2016-03-10 14:39:14 +0100

    add hmdb51 code

--- examples/hmdb51/dataset_file_examples/gen_iterator_metafile.py
+++ examples/hmdb51/dataset_file_examples/gen_iterator_metafile.py
@@ -9,10 +9,10 @@ import glob
 
 if __name__ == "__main__":
 
-    trainset = './train_flow_split2.txt'
-    fp_train_filenames = open('/home/zhenyang/Workspace/data/HMDB51/train_filenames_split2.txt', 'w')
-    fp_train_framenum = open('/home/zhenyang/Workspace/data/HMDB51/train_framenum_split2.txt', 'w')
-    fp_train_labels = open('/home/zhenyang/Workspace/data/HMDB51/train_labels_split2.txt', 'w')
+    trainset = './train_flow_split3.txt'
+    fp_train_filenames = open('/home/zhenyang/Workspace/data/HMDB51/train_filenames_split3.txt', 'w')
+    fp_train_framenum = open('/home/zhenyang/Workspace/data/HMDB51/train_framenum_split3.txt', 'w')
+    fp_train_labels = open('/home/zhenyang/Workspace/data/HMDB51/train_labels_split3.txt', 'w')
     with open(trainset) as fp:
         for line in fp:
             splits = line.rstrip().split(' ')
@@ -25,10 +25,10 @@ if __name__ == "__main__":
             fp_train_labels.write(str(label)+'\n')
 
 
-    testset = './val_flow_split2.txt'
-    fp_test_filenames = open('/home/zhenyang/Workspace/data/HMDB51/test_filenames_split2.txt', 'w')
-    fp_test_framenum = open('/home/zhenyang/Workspace/data/HMDB51/test_framenum_split2.txt', 'w')
-    fp_test_labels = open('/home/zhenyang/Workspace/data/HMDB51/test_labels_split2.txt', 'w')
+    testset = './val_flow_split3.txt'
+    fp_test_filenames = open('/home/zhenyang/Workspace/data/HMDB51/test_filenames_split3.txt', 'w')
+    fp_test_framenum = open('/home/zhenyang/Workspace/data/HMDB51/test_framenum_split3.txt', 'w')
+    fp_test_labels = open('/home/zhenyang/Workspace/data/HMDB51/test_labels_split3.txt', 'w')
     with open(testset) as fp:
         for line in fp:
             splits = line.rstrip().split(' ')
